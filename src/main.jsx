e493bb6ae0a7ca9d12cf2e9118ed2a0823d148be
Joao Paulo Souza Colombo <148556778+JoaoPauloColombo@users.noreply.github.com>
2024-09-02 11:42:13 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
+// Importa o StrictMode e createRoot do React, e o componente principal App
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
+// Cria a raiz do aplicativo e renderiza o componente App dentro do StrictMode
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
