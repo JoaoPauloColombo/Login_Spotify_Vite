f4ba07eb05d6d01763f530047d460033e81ad7d4
Joao Paulo Souza Colombo <148556778+JoaoPauloColombo@users.noreply.github.com>
2024-09-02 11:41:55 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
+// Importa os componentes Header, Forms, e Footer, além do arquivo de vídeo e o arquivo de estilos CSS
 import Header from './Components/Header';
 import Forms from './Components/Forms';
 import Footer from './Components/Footer';
-import video from './assets/Video.mp4'
+import video from './assets/Video.mp4';
 import './Style/App.css';
 
+// Define o componente App
 function App() {
-
   return (
     <>
-    <div className="video-background">
-    <video autoPlay muted loop id="bg-video">
-      <source src={video} type="video/mp4" />
-    </video>
-  </div>
+      {/* Contêiner para o vídeo de fundo */}
+      <div className="video-background">
+        <video autoPlay muted loop id="bg-video">
+          <source src={video} type="video/mp4" />
+        </video>
+      </div>
+      
+      {/* Contêiner para o cabeçalho */}
       <div className="header-container">
         <Header />
       </div>
+      
+      {/* Contêiner para o formulário */}
       <div className="forms-container">
         <Forms />
       </div>
+      
+      {/* Contêiner para o rodapé */}
       <div className="footer-container">
         <Footer />
       </div>
-
     </>
   );
-};
+}
 
+// Exporta o componente App para ser usado como o principal na aplicação
 export default App;
